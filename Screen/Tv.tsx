c4dc52106439bd037b491a27c5ed2e2cd51ef404
heesungjang <heesungj7@gmail.com>
2022-01-01 22:15:31 +0900

    tv flat lists added

--- Screen/Tv.tsx
+++ Screen/Tv.tsx
@@ -1,11 +1,72 @@
+//REACT
 import React from "react";
 
-import { View, Text } from "react-native";
+import { View, Text, FlatList, ScrollView } from "react-native";
+import { useQuery } from "react-query";
+import { tvApi } from "../api";
+import { Loader } from "../components/Loader";
+import VMedia from "../components/VMedia";
 
-const Tv = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Tv</Text>
-  </View>
-);
+const Tv: React.FC = () => {
+  const { isLoading: airingTodayLoading, data: airingTodayData } = useQuery(
+    ["tv", "today"],
+    tvApi.airingToday
+  );
+  const { isLoading: topRatedLoading, data: topRatedData } = useQuery(
+    ["tv", "top"],
+    tvApi.topRated
+  );
+  const { isLoading: trendingLoading, data: trendingData } = useQuery(
+    ["tv", "trending"],
+    tvApi.trending
+  );
+
+  const loading = airingTodayLoading || topRatedLoading || trendingLoading;
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <ScrollView>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={airingTodayData.results}
+        renderItem={({ item }) => (
+          <VMedia
+            posterPath={item.poster_path}
+            originalTitle={item.original_name}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={topRatedData.results}
+        renderItem={({ item }) => (
+          <VMedia
+            posterPath={item.poster_path}
+            originalTitle={item.original_name}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={trendingData.results}
+        renderItem={({ item }) => (
+          <VMedia
+            posterPath={item.poster_path}
+            originalTitle={item.original_name}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+    </ScrollView>
+  );
+};
 
 export default Tv;
